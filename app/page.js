54aa32e0789410b2ad6aa622634e1a660b74ca3b
Christopher Rodriguez <62278746+chrisrod115@users.noreply.github.com>
2026-06-25 15:12:38 -0700

    Updated hero section.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="btn">Click me</main>;
+  return (
+    <main>
+      <h1>Collect feedback to build better products</h1>
+      <div>
+        Create a feedback board in minutes, prioritize features and build
+        products your customers will love!
+      </div>
+    </main>
+  );
 }
