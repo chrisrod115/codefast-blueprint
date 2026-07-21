b063a9be40fe951eb244d00c593050387fc26080
Christopher Rodriguez <chrisrod115@gmail.com>
2026-07-20 19:41:07 -0700

    Add Google provider and MongoDB adapter to NextAuth configuration

--- auth.js
+++ auth.js
@@ -1,7 +1,16 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "./libs/mongo";
+import Google from "next-auth/providers/google";
 
 const config = {
-  providers: [],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
