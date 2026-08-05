705edec6019a137fe5b498971a9a2977bc47ce4a
Christopher Rodriguez <chrisrod115@gmail.com>
2026-08-04 21:17:33 -0700

    Add FeedbackBoard component to fetch and display board details

---
+++ app/dashboard/b/[boardId]/page.js
@@ -0,0 +1,23 @@
+import connectMongo from "@/libs/mongoose";
+import Board from "@/models/Board";
+import { auth } from "@/auth";
+import redirect from "next/navigation";
+
+const getBoard = async (boardId) => {
+  const session = await auth();
+  await connectMongo();
+  const board = await Board.findOne({
+    _id: boardId,
+    userId: session?.user?.id,
+  });
+  if (!board) {
+    redirect("/dashboard");
+  }
+  return board;
+};
+
+export default async function FeedbackBoard({ params }) {
+  const { boardId } = params;
+  const board = await getBoard(boardId);
+  return <main>{board.name}</main>;
+}
