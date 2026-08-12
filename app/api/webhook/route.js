817d54ffcf8f857e4b63765b837f9e433bfdb342
Christopher Rodriguez <62278746+chrisrod115@users.noreply.github.com>
2026-08-12 06:17:46 -0700

    Implement Stripe webhook handling for checkout session completion

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,2 +1,33 @@
 import { NextResponse } from "next/server";
-export async function POST(req) {}
+import { headers } from "next/headers";
+import Stripe from "stripe";
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
+export async function POST(req) {
+  try {
+    const stripe = new Stripe(process.env.STRIPE_API_KEY);
+
+    const body = await req.text();
+    const signature = headers().get("stripe-signature");
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      webhookSecret,
+    );
+
+    const { data, type } = event;
+
+    if (type === "checkout.session.completed") {
+      await connectMongo();
+      const user = await User.findById(data.object.client_reference_id);
+      user.hasAccess = true;
+      user.customerId = data.object.customer;
+
+      await user.save();
+    }
+  } catch (e) {
+    console.error("Stripe Error: " + e?.message);
+  }
+  return NextResponse.json({});
+}
